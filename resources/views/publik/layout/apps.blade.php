1659a1028a9cdc5356c36cbbcac74584562515cc
syhnnzhra <syhnnzhra@gmail.com>
2021-01-13 21:03:02 +0700

    cart

--- resources/views/publik/layout/apps.blade.php
+++ resources/views/publik/layout/apps.blade.php
@@ -8,7 +8,6 @@
 	<link rel="stylesheet" href="{{asset('assets2/css/slick.css')}}">
 	<link href="{{asset('assets2/css/bootstrap.css')}}" rel="stylesheet">
 	<link href="{{asset('assets2/js/jequery.js')}}" rel="stylesheet">
-	
 
 	<title>@yield('title')</title>
 	<!-- link online -->
@@ -21,19 +20,18 @@
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
 	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-	<meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-	
 
 	<!-- end link -->
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
+
+	<style>
+		.body{
+			background-color: #E3DED8;
+		}
+	</style>
 
 </head>
 <body>
@@ -63,7 +61,10 @@
                 </div>
                 @endguest
 			<a href="">
-			<i class='fas fa-shopping-cart mt-2' style='font-size:25px ;color: #c18f59;'></i></a>
+				<a href="{{ route('front.list_cart') }}" class="icons">
+					<i class="fas fa-shopping-cart mt-2" style='font-size:25px ;color: #c18f59;'></i>
+				</a>
+				<!-- <i class='fas fa-shopping-cart mt-2' style='font-size:25px ;color: #c18f59;'></i></a> -->
 			<div class="demo-2 search mr-auto ml-3">
 				<form>
 					<span class="icon"><i class="fa fa-search" style='font-size:25px ;color: #c18f59;'></i></span>
